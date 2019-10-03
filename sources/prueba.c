fa0190b964b2be8ab08e67d5a2bdcf45a30fe693
SakoPako <correopaco2000@gmail.com>
2019-10-03 11:49:10 +0200

    inicio recurso y doc

---
+++ sources/prueba.c
@@ -0,0 +1,27 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include "fusion-c/header/msx_fusion.h"
+#include "fusion-c/header/io.h"
+//#include "fusion-c/header/vars_msxSystem.h"
+
+void main() {
+	//char afbdata[AFB_SIZE];
+  	//Print ("probando ...");
+  	//FT_LoadData("prueba.txt", afbdata, AFB_SIZE, 0);
+  	//Print (afbdata);
+
+
+char sbuf[10]; // Set a 10 bytes buffer
+int fH; // Set a file handler variable
+
+FCBlist *FCB = FCBs(); // FCB initialization
+
+fH = open("prueba.txt", O_RDWR ); // open file for read
+read(fH, sbuf, 10); // Read 10 bytes to 74
+close(fH); // Close file
+
+Print (sbuf);
+	
+
+} /////////////////////////
