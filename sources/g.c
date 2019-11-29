d6abb2ce74491e71059e1056817d0e295c4c2bc9
SakoPako <correopaco2000@gmail.com>
2019-11-29 18:17:40 +0100

    POSICION Y MOV DE ENEM ESCENA 1
    INICIO SÓLO SUPERIOR Y FALTA DEPURAR

--- sources/g.c
+++ sources/g.c
@@ -10,15 +10,18 @@
 // ESTRUCTURAS GENERICAS
 // EL PROTA ES UN CONJUNTO DE 4 SPRITES (LAS PRIERNAS SON DOS), EL RESTO 1 (PUEDEN CAMBIAR SI SON ESCENAS)
 typedef struct {
-	BYTE tipo; // SE UTILIZA SOBRE TODO CON LOS PERSONAJES QUE NO SON EL PROTA U OBJETO QUE SE MUEVEN
+	BYTE tipo; // SE UTILIZA SOBRE TODO CON LOS PERSONAJES QUE NO SON EL PROTA U OBJETO QUE SE MUEVEN (1 GREM BUENO)
 	BYTE activo; // SE UTILIZA SOBRE TODO CON LOS PERSONAJES QUE NO SON EL PROTA U OBJETO QUE SE MUEVEN
-	BYTE direccion_mira; // LA DIRECCIÓN A LA QUE MIRA EL ESPRITE (EN PRINCIPIO SÓLO VALE PROTA): 0 IZQ - 1 DER
+	BYTE direccionMira; // LA DIRECCIÓN A LA QUE MIRA EL ESPRITE (EN PRINCIPIO SÓLO VALE PROTA): 0 IZQ - 1 DER
 	BYTE x; // POS X
 	BYTE y; // POS Y
-	BYTE velocidadx; // PIXELS QUE MOVERÁ EN X EN CADA ITERACIÓN
-	BYTE velocidady; // PIXELS QUE MOVERÁ EN Y EN CADA ITERACIÓN
-	BYTE cont_siguiente_escena; // DE N A 0 (CUANDO LLEGA A 0 CAMBIO DE ESCENA)
-	BYTE reset_contador; // N PARA EL CONTADOR
+	BYTE destinoX; // CUANDO TIENE QUE LLEGAR A UNA COORDENADA X DESDE DONDE ESTÉ EL SPRITE
+	BYTE destinoY; // CUANDO TIENE QUE LLEGAR A UNA COORDENADA Y DESDE DONDE ESTÉ EL SPRITE
+	BYTE velocidadX; // PIXELS QUE MOVERÁ EN X EN CADA ITERACIÓN
+	BYTE velocidadY; // PIXELS QUE MOVERÁ EN Y EN CADA ITERACIÓN
+	BYTE contSiguienteEscena; // DE N A 0 (CUANDO LLEGA A 0 CAMBIO DE ESCENA)
+	BYTE resetContador; // N PARA EL CONTADOR
+	BYTE plano; // PARA ASIGNAR UN PLANO AL SPRITE
 	// EL ESPRITE MÁS COMPLEJO LLEVARÁ 3 ESCENAS PARA IZQ Y 3 PARA DER
 	BYTE escena1; // EL PATRON DE SPRITES A MOSTRAR ARRIBA IZQ
 	BYTE escena2; // EL PATRON DE SPRITES A MOSTRAR ABAJO IZQ
@@ -76,6 +79,8 @@ void PonerTextosNivel				(void);
 void PonerTextosEscena				(void);
 void PonerTextosContador			(void);
 void PonerMarcoContador 			(void);
+void inicializaSpriteProta			(BYTE escena);
+void inicializaSpriteEnemigo		(BYTE escena, Sprites_STR* enemigo);
 void MueveProta						(BYTE escena, BYTE direccion);
 void FlipSpritesProta				(void);
 BYTE SuperaLimitesProtaEsc1			(void);
@@ -117,7 +122,7 @@ void main(void)
 	do { 
 		// PINTAR PANTALLA INICIAL DE JUEGO
 		//#######  comentado para ahorrar tiempo
-		PintarPantallaInicialJuego();
+		//PintarPantallaInicialJuego();
 
 		// SELECCIONAR SI APLICA MODOS DE JUEGO
 		// SETUP / INICIALIZACIÓN (NO INICIAL, DE CADA PARTIDA) VARIABLES DE JUEGO
@@ -132,7 +137,7 @@ void main(void)
 			// PINTAR INTRO DE ESCENA
 			
 			//#######  comentado para ahorrar tiempo
-			PintarIntroEscena();
+			//PintarIntroEscena();
 
 			switch(escena) {
 				case ESCENA1: {
@@ -141,52 +146,31 @@ void main(void)
 
 					// CARGAR GRÁFICOS Y PATRONES NO COMUNES (DE ESCENA)
 					// PONER LOS PATRONES DE SPRITES DE ESTA ESCENA
-					
-						SetSpritePattern((char)0,  prota1_izq_1,   (char)32);
-						SetSpritePattern((char)4,  prota1_izq_2,   (char)32);
-						SetSpritePattern((char)8,  prota1_izq_3,   (char)32);
-						SetSpritePattern((char)12, prota1_izq_41,  (char)32);
-						SetSpritePattern((char)16, prota1_izq_42,  (char)32);
-						SetSpritePattern((char)20, prota1_der_1,   (char)32);
-						SetSpritePattern((char)24, prota1_der_21,  (char)32);
-						SetSpritePattern((char)28, prota1_der_22,  (char)32);
-						SetSpritePattern((char)32, prota1_der_3,   (char)32);
-						SetSpritePattern((char)36, prota1_der_4,   (char)32);				
-						SetSpritePattern((char)40, grenbueno_izq_1,(char)32);
-						SetSpritePattern((char)44, grenbueno_izq_2,(char)32);
-						SetSpritePattern((char)48, grenbueno_der_1,(char)32);
-						SetSpritePattern((char)52, grenbueno_der_2,(char)32);
-						SetSpritePattern((char)56, grenbueno_cae,  (char)32);
+						// DEFINE PATRONES DE SPRITES
+						SetSpritePattern((char)0,  prota1_izq_1,   	(char)32);
+						SetSpritePattern((char)4,  prota1_izq_2,   	(char)32);
+						SetSpritePattern((char)8,  prota1_izq_3,   	(char)32);
+						SetSpritePattern((char)12, prota1_izq_41,  	(char)32);
+						SetSpritePattern((char)16, prota1_izq_42,  	(char)32);
+						SetSpritePattern((char)20, prota1_der_1,   	(char)32);
+						SetSpritePattern((char)24, prota1_der_21,  	(char)32);
+						SetSpritePattern((char)28, prota1_der_22,  	(char)32);
+						SetSpritePattern((char)32, prota1_der_3,   	(char)32);
+						SetSpritePattern((char)36, prota1_der_4,   	(char)32);				
+						SetSpritePattern((char)40, grenbueno_izq_1,	(char)32);
+						SetSpritePattern((char)44, grenbueno_izq_2,	(char)32);
+						SetSpritePattern((char)48, grenbueno_der_1,	(char)32);
+						SetSpritePattern((char)52, grenbueno_der_2,	(char)32);
+						SetSpritePattern((char)56, grenbueno_cae,  	(char)32);
+						SetSpritePattern((char)60, grenbueno_tiraobj,(char)32);
 
-
-						sprites_prota.activo = (BYTE)1;
-						sprites_prota.direccion_mira = (BYTE)PROTAMIRAIZQ;
-						sprites_prota.x = (BYTE)100;
-						sprites_prota.y = (BYTE)100;
-						sprites_prota.velocidadx = (BYTE)1;
-						sprites_prota.velocidady = (BYTE)0;
-						sprites_prota.cont_siguiente_escena = (BYTE)2;
-						sprites_prota.reset_contador = (BYTE)2;
-						sprites_prota.escena1  = (BYTE)0;
-						sprites_prota.escena2  = (BYTE)4;
-						sprites_prota.escena3  = (BYTE)8;
-						sprites_prota.escena4  = (BYTE)12;
-						sprites_prota.escena1i = (BYTE)0;
-						sprites_prota.escena2i = (BYTE)4;
-						sprites_prota.escena3i = (BYTE)8;
-						sprites_prota.escena4i = (BYTE)12;
-						sprites_prota.escena5i = (BYTE)12;
-						sprites_prota.escena6i = (BYTE)16;
-						sprites_prota.escena1d = (BYTE)20;
-						sprites_prota.escena2d = (BYTE)24;
-						sprites_prota.escena3d = (BYTE)32;
-						sprites_prota.escena4d = (BYTE)36;
-						sprites_prota.escena5d = (BYTE)24;
-						sprites_prota.escena6d = (BYTE)28;
+						// INICIOALIZA ESTRUCTURA DE SPRITE DE PROTA						
+						inicializaSpriteProta((BYTE)escena);
   
+						// RESERVA MEMORIA PARA ESTRUCTURAS DE SPRITES (NO PROTA)
+						sprites_otros = (Sprites_STR *) malloc(1); // LA MISMA ESTRUCTURA NOS VALE PARA GREM ANDANDO A IZQ, DER, CAER O MACETA QUE CAE
 
-						sprites_otros = (Sprites_STR *) malloc(3); // SE RESERVA MEMORIA POARA DOS SPRITES GREMLIN BUENO ANDANDO O CAYENDO (2) EN ESTA ESCENA Y OBJETO QUE PUEDE CAER (1)
-
+						inicializaSpriteEnemigo((BYTE)escena, sprites_otros); 
 
 					// PINTAR EN PANTALLA
   					HideDisplay(); // OCULTAMOS PORQUE AL PINTAR LOS TILES SE VEN EN PANTALLA
@@ -210,7 +194,27 @@ void main(void)
 						// MOVIMIENTO
 						MueveProta(escena,JoystickRead(LEETECLADO));
 
-						// INICIALIZA/SPAWNEA ENEMIGOS				
+						// INICIALIZA/SPAWMEA/MUEVE ENEMIGOS
+						if (sprites_otros[0].activo == FALSO) { // INICIALIZA/SPAWMEA
+							sprites_otros[0].activo = VERDADERO;
+							sprites_otros[0].destinoX = (BYTE)FT_RandomNumber((BYTE)BASESPRITEGRENHORIZQ, (BYTE)BASESPRITEGRENHORDER);
+							if(FT_RandomNumber((BYTE)0, (BYTE)2) == (BYTE)1) {
+								sprites_otros[0].direccionMira = MIRADER;
+								sprites_otros[0].x = BASESPRITEGRENHORDER;
+								sprites_otros[0].escena1 = sprites_otros[0].escena1d;
+							} else {
+								sprites_otros[0].direccionMira = MIRAIZQ;
+								sprites_otros[0].x = BASESPRITEGRENHORIZQ;
+								sprites_otros[0].escena1 = sprites_otros[0].escena1i;
+							}
+							sprites_otros[0].y = BASESPRITEGRENVER;
+						} else { // MUEVE ENEMIGOS
+							if (sprites_otros[0].direccionMira == MIRADER) {
+								sprites_otros[0].x += sprites_otros[0].velocidadX;
+							} else {
+								sprites_otros[0].x += -sprites_otros[0].velocidadX;
+							}
+						}
 
 						// DETECCIÓN DE LIMITES DE OBJETOS MÓVILES
 					
@@ -220,11 +224,39 @@ void main(void)
 					
 						// DIBUJAR CAMBIOS EN PANTALLA
 							// PINTAR/ACTUALIZAR ENEMIGOS, SI APLICA
+							if (sprites_otros[0].activo == VERDADERO) {
+								PutSprite(sprites_otros[0].plano, sprites_otros[0].escena1, sprites_otros[0].x, sprites_otros[0].y, COLORROJOOSCURO);
+
+
+								if (sprites_otros[0].direccionMira == MIRADER) {
+									if (sprites_otros[0].x > sprites_otros[0].destinoX)
+										sprites_otros[0].activo = FALSO;
+								} else {
+									if (sprites_otros[0].x < sprites_otros[0].destinoX)
+										sprites_otros[0].activo = FALSO;
+								}
+
+
+							}
+
+
+
+
+
+/* // DEJO ESTO COMENTADO PORQUE AUNQUE NO SERÁ DE UTILIDAD EN LA ESCENA 1 ES LA BASE PARA EL RESTO DE ESCENAS CON MÁS DE UN ENEMIGO (PARA COPIARLO MÁS TARDE)
+							for (contador = 0; contador < 3; contador++) {
+								if (sprites_otros[contador].activo) {
+
+								}
+							}
+*/
 							// PINTAR/ACTUALIZAR ELEMENTOS SI APLICA
 							// PINTAR/ACTUALIZAR ACCESORIOS, SIAPLICA
 							// ACTUALIZA ANIMACIONES/ ACCESORIOS
 
 						// OTROS CHEQUEOS Y ACTUALIZCIONES DE VARIABLES
+
+
 
 					} while(contadorEscena); // FIN LOOP DENTRO ESCENA (CONTADOR ESCENA TIENDE A 0 Y VERO ES FALSO)
 
@@ -444,6 +476,70 @@ void main(void)
 } // FIN PROGRAMA
 
 
+// FUNCION: INICIALIZA LOS VALORES DE LA ESTRUCTURA DEL SPRITE DEL PROTA
+// ENTRADAS: 
+// escena: LA ESCENA DEL JUEGO QUE SE TRATE YA QUE PUEDE VARIAR LOS PATRONES A USAR
+// SALIDAS: -
+void inicializaSpriteProta(BYTE escena) {
+	switch(escena){
+		case ESCENA1: {
+			sprites_prota.direccionMira = (BYTE)MIRAIZQ;
+			sprites_prota.x = (BYTE)100;
+			sprites_prota.y = (BYTE)100;
+			sprites_prota.velocidadX = (BYTE)1;
+			sprites_prota.velocidadY = (BYTE)0;
+			sprites_prota.contSiguienteEscena = (BYTE)2;
+			sprites_prota.resetContador = (BYTE)2;
+			sprites_prota.escena1  = (BYTE)0;
+			sprites_prota.escena2  = (BYTE)4;
+			sprites_prota.escena3  = (BYTE)8;
+			sprites_prota.escena4  = (BYTE)12;
+			sprites_prota.escena1i = (BYTE)0;
+			sprites_prota.escena2i = (BYTE)4;
+			sprites_prota.escena3i = (BYTE)8;
+			sprites_prota.escena4i = (BYTE)12;
+			sprites_prota.escena5i = (BYTE)12;
+			sprites_prota.escena6i = (BYTE)16;
+			sprites_prota.escena1d = (BYTE)20;
+			sprites_prota.escena2d = (BYTE)24;
+			sprites_prota.escena3d = (BYTE)32;
+			sprites_prota.escena4d = (BYTE)36;
+			sprites_prota.escena5d = (BYTE)24;
+			sprites_prota.escena6d = (BYTE)28;
+			break;
+		}
+	}
+} // FIN inicializaSpriteProta
+
+
+// FUNCION: INICIALIZA LOS VALORES DE LA ESTRUCTURA SE SPRITES NO PROTA SEGÚN ESCENA Y TIPO¿?
+// ENTRADAS: 
+// escena: LA ESCENA DEL JUEGO QUE SE TRATE YA QUE PUEDE VARIAR LOS PATRONES A USAR
+// escena: ESTRUCTURA DE ESPRITE A INICIALIZAR, SE PASA COMO REFERENCIA PARA PODER MODIFICAR
+// SALIDAS: -
+void inicializaSpriteEnemigo (BYTE escena, Sprites_STR* enemigo) {
+	switch(escena){
+		case ESCENA1: {
+			enemigo->tipo = (BYTE)1; // GREMBLIN BUENO
+			enemigo->activo = (BYTE)FALSO;
+			enemigo->direccionMira = (BYTE)MIRAIZQ;
+			enemigo->velocidadX = (BYTE)VELPROTA1;
+			enemigo->contSiguienteEscena = (BYTE)2;
+			enemigo->resetContador = (BYTE)2;
+			enemigo->plano = (BYTE)5;
+			enemigo->escena1 = (BYTE)48;
+			enemigo->escena1i = (BYTE)48;
+			enemigo->escena2i = (BYTE)52;
+			enemigo->escena1d = (BYTE)40;
+			enemigo->escena2d = (BYTE)44;
+			enemigo->escena5d = (BYTE)56; // grenbueno_cae
+			enemigo->escena6d = (BYTE)60; // grenbueno_tiraobj
+			break;
+		}
+	}
+}
+
+
 // FUNCION: ACTUALIZA LOS DATOS DE LOS SPRITES DEL PROTA Y MUESTRA AL PROTA POR PANTALLA
 // ENTRADAS: 
 // escena: LA ESCENA DEL JUEGO QUE SE TRATE (EL MOVIMIENTO DEL PROTA PUEDE VARIAR)
@@ -456,18 +552,18 @@ void MueveProta (BYTE escena, BYTE direccion) {
 	if (escena == 1) {
 		switch(direccion) {
 			case TIZQUIERDA: {
-				if (sprites_prota.direccion_mira == PROTAMIRADER)
+				if (sprites_prota.direccionMira == MIRADER)
 					FlipSpritesProta();
 
 				if(SuperaLimitesProtaEsc1() == FALSO) // SI ES VERDADERO SUPERA LIMITES Y NO MOVEMOS SPRITE PROTA
-					sprites_prota.x += -sprites_prota.velocidadx;
+					sprites_prota.x += -sprites_prota.velocidadX;
 				else
 					sprites_prota.x = LIMITEPROESC1IZQ;
 
-				sprites_prota.cont_siguiente_escena--;
+				sprites_prota.contSiguienteEscena--;
 
-				if (sprites_prota.cont_siguiente_escena == 0) {
-					sprites_prota.cont_siguiente_escena = sprites_prota.reset_contador;
+				if (sprites_prota.contSiguienteEscena == 0) {
+					sprites_prota.contSiguienteEscena = sprites_prota.resetContador;
 				
 					if (sprites_prota.escena4 == sprites_prota.escena5i)
 						sprites_prota.escena4 = sprites_prota.escena6i;
@@ -478,18 +574,18 @@ void MueveProta (BYTE escena, BYTE direccion) {
 			}
 
 			case TDERECHA: {
-				if (sprites_prota.direccion_mira == PROTAMIRAIZQ)
+				if (sprites_prota.direccionMira == MIRAIZQ)
 					FlipSpritesProta();
 				
 				if(SuperaLimitesProtaEsc1() == FALSO) // SI ES VERDADERO SUPERA LIMITES Y NO MOVEMOS SPRITE PROTA
-					sprites_prota.x += sprites_prota.velocidadx;
+					sprites_prota.x += sprites_prota.velocidadX;
 				else
 					sprites_prota.x = LIMITEPROESC1DER;
 
-				sprites_prota.cont_siguiente_escena--;
+				sprites_prota.contSiguienteEscena--;
 
-				if (sprites_prota.cont_siguiente_escena == 0) {
-					sprites_prota.cont_siguiente_escena = sprites_prota.reset_contador;
+				if (sprites_prota.contSiguienteEscena == 0) {
+					sprites_prota.contSiguienteEscena = sprites_prota.resetContador;
 				
 					if (sprites_prota.escena2 == sprites_prota.escena5d)
 						sprites_prota.escena2 = sprites_prota.escena6d;
@@ -508,31 +604,31 @@ void MueveProta (BYTE escena, BYTE direccion) {
 } // FIN MueveProta
 
 
-// FUNCION: EXAMINA LOS DATOS DE LOS SPRITES DEL PROTA. SI EL PROTA MIRA A LA DERECHA O LA IZQ (sprites_prota.direccion_mira = PROTAMIRAIZQ o PROTAMIRADER)
+// FUNCION: EXAMINA LOS DATOS DE LOS SPRITES DEL PROTA. SI EL PROTA MIRA A LA DERECHA O LA IZQ (sprites_prota.direccionMira = PROTAMIRAIZQ o PROTAMIRADER)
 // LOS DATOS EN LA ESTRUCTURA PARA QUE MIRE A LA IZQUIERDA
-// ENTRADAS: - (SE PODRÍA HABER PASADO sprites_prota.direccion_mira PERO APROVECHO QUE ES GLOBAL)
+// ENTRADAS: - (SE PODRÍA HABER PASADO sprites_prota.direccionMira PERO APROVECHO QUE ES GLOBAL)
 // SALIDAS: -
 void FlipSpritesProta (void) {
-	if (sprites_prota.direccion_mira == PROTAMIRAIZQ) { // INTERCAMBIAMOS POSICIONES CON LOS DE LA DERECHA
+	if (sprites_prota.direccionMira == MIRAIZQ) { // INTERCAMBIAMOS POSICIONES CON LOS DE LA DERECHA
 		sprites_prota.escena1 = sprites_prota.escena1d;
 		sprites_prota.escena2 = sprites_prota.escena2d;
 		sprites_prota.escena3 = sprites_prota.escena3d;
 		sprites_prota.escena4 = sprites_prota.escena4d;
-		sprites_prota.direccion_mira = PROTAMIRADER;
+		sprites_prota.direccionMira = MIRADER;
 	} else {  // INTERCAMBIAMOS POSICIONES CON LOS DE LA IZQUIERDA	
 		sprites_prota.escena1 = sprites_prota.escena1i;
 		sprites_prota.escena2 = sprites_prota.escena2i;
 		sprites_prota.escena3 = sprites_prota.escena3i;
 		sprites_prota.escena4 = sprites_prota.escena4i;
-		sprites_prota.direccion_mira = PROTAMIRAIZQ;
+		sprites_prota.direccionMira = MIRAIZQ;
 	}
 } // FIN FlipSpritesProta
 
 // FUNCION: EXAMINA LOS DATOS DE LOS SPRITES DEL PROTA. SI NO SUPERA LOS LIMITES MANDO FALSO (0) Y EN CASO CONTRARIO VERDADERO (0)
-// ENTRADAS: - (SE PODRÍA HABER PASADO sprites_prota.direccion_mira PERO APROVECHO QUE ES GLOBAL)
+// ENTRADAS: - (SE PODRÍA HABER PASADO sprites_prota.direccionMira PERO APROVECHO QUE ES GLOBAL)
 // SALIDAS: SI NO SUPERA LOS LIMITES MANDO FALSO (0) Y EN CASO CONTRARIO VERDADERO (0)
 BYTE SuperaLimitesProtaEsc1 (void) {
-	if (sprites_prota.direccion_mira == PROTAMIRAIZQ) {
+	if (sprites_prota.direccionMira == MIRAIZQ) {
 		if (sprites_prota.x <= LIMITEPROESC1IZQ)
 			return VERDADERO;
 	} else {
